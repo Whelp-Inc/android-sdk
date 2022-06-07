b8ea0b539421787c100057443dccf37156cf426f
yunisrasulzade <yunisrasulzada99@gmail.com>
2022-06-07 13:29:40 +0400

    Android 10+ query permission

--- whelp/src/main/java/com/whelp/model/UserCredential.kt
+++ whelp/src/main/java/com/whelp/model/UserCredential.kt
@@ -5,7 +5,5 @@ data class UserCredentials(val language: String, val contact: Contact)
 data class Contact(
     val email: String,
     val fullname: String,
-    val phone: String,
-//    val account_type: String,
-//    val pricing_plan: String
+    val phone: String
 )
