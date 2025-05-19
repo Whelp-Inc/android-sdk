9e631fe219aa0c3df75e7e810e26f303d7b8d3c2
faridGuliyew <faridg@crocusoft.com>
2025-05-19 16:39:52 +0400

    - Redundant logout method removed

--- whelp/src/main/java/com/whelp/data/ApiService.kt
+++ whelp/src/main/java/com/whelp/data/ApiService.kt
@@ -1,18 +1,13 @@
 package com.whelp.data
 
 import com.whelp.model.AuthResponse
-import io.reactivex.Single
-import io.reactivex.Completable
 import okhttp3.RequestBody
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
 
     @POST("auth")
-    suspend fun auth(@Body auth: RequestBody?): AuthResponse
-
-    @POST("logout")
-    suspend fun logout()
+    suspend fun auth(@Body auth: RequestBody?): Response<AuthResponse>
 }
